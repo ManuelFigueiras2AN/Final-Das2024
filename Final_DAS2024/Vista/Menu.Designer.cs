358b8a64bcacc3028376b6c39fe5b933237447bb
manuel figueiras <manufigueiras0@hotmail.com>
2024-12-10 16:03:56 -0300

    Prueba Menu fondo

--- Final_DAS2024/Vista/Menu.Designer.cs
+++ Final_DAS2024/Vista/Menu.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Menu";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            mdp_Background = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)mdp_Background).BeginInit();
+            SuspendLayout();
+            // 
+            // mdp_Background
+            // 
+            mdp_Background.Enabled = true;
+            mdp_Background.Location = new Point(-7, -1);
+            mdp_Background.Name = "mdp_Background";
+            mdp_Background.OcxState = (AxHost.State)resources.GetObject("mdp_Background.OcxState");
+            mdp_Background.Size = new Size(815, 450);
+            mdp_Background.TabIndex = 0;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(mdp_Background);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Menu";
+            Text = "Menu";
+            Load += Menu_Load;
+            ((System.ComponentModel.ISupportInitialize)mdp_Background).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer mdp_Background;
     }
 }
--- Final_DAS2024/Vista/Menu.cs
+++ Final_DAS2024/Vista/Menu.cs
@@ -16,5 +16,24 @@ namespace Vista
         {
             InitializeComponent();
         }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            ConfigurarVideo();
+        }
+
+        private void ConfigurarVideo()
+        {
+            string videoPath = "C:\\Users\\Ente\\source\\repos\\Final-Das2024\\Final_DAS2024\\Vista\\Properties\\Resources\\Videos\\fondo.mp4"; // Asegúrate de colocar la ruta correcta del video
+
+            // Reproduce el video en el fondo
+            mdp_Background.URL = videoPath;
+            mdp_Background.Ctlcontrols.play();
+            mdp_Background.settings.setMode("loop", true);  // Opcional: Hacer que el video se repita en bucle
+            mdp_Background.uiMode = "None";
+            
+            // Poner el control de video en segundo plano
+            mdp_Background.SendToBack();
+        }
     }
 }
